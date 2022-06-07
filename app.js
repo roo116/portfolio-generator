b604aade44c801098288449187a12558191d3ea5
roo116 <roo116@nc.rr.com>
2022-06-07 02:15:02 -0400

    function that receives command-line arguement and inserts them into HTML.

--- app.js
+++ app.js
@@ -1,1 +1,24 @@
-const profileDataArgs = process.argv.slice(2);
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+const [name, github] = profileDataArgs;
+
+const generatePage = (userName, githubName) => {
+    return `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+<h1>${name}</h1>
+<h2><a href="https://github.com/${github}">GitHub</a></h2>
+</body>
+</html>
+    `;
+};
+
+console.log(name, github);
+console.log(generatePage(name, github));
