632ff77764ff0c040bd1368911815f326d05add7
roo116 <roo116@nc.rr.com>
2022-06-09 16:39:43 -0400

    finished styling

--- app.js
+++ app.js
@@ -138,12 +138,42 @@ Add a New Project
 promptUser()
   .then(promptProject)
   .then((portfolioData) => {
-    // will be uncommented in lesson 4
-    const pageHTML = generatePage(portfolioData);
-    fs.writeFile("./index.html", pageHTML, (err) => {
-      if (err) throw new Error(err);
-      console.log(
-        "Page created! Check out index.html in this directory to see it!"
-      );
-    });
+    return generatePage(portfolioData);
+  })
+
+  .then((pageHTML) => {
+    return writeFile(pageHTML);
+  })
+
+  .then((writeFileResponse) => {
+    console.log(writeFileResponse);
+    return fs.copyFile();
+  })
+
+  .then((copyFileResponse) => {
+    console.log(copyFileResponse);
+    return fs.copyFile();
   });
+// will be uncommented in lesson 4
+const pageHTML = generatePage(portfolioData);
+
+fs.writeFile("./dist/index.html", pageHTML, (err) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(
+    "Page created! Check out index.html in this directory to see it!"
+  );
+});
+
+fs.copyFile(".src.style.css", "./dist/style.css", (err) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  console.log("Style sheet copied successfully");
+});
+
+//
